2d8242cd5ed0100b48a27dca6562ad4b0901003c
Juan Manuel Borges Caño <juanmabcmail@gmail.com>
2013-12-03 14:04:28 +0100

    Tweaks

--- imgs/imgs-fortune.in.sh
+++ imgs/imgs-fortune.in.sh
@@ -2,9 +2,9 @@
 
 # November 22nd, 2013, Juan Manuel Borges Caño
 
-word="$(shuf /usr/share/dict/words | head -1)"
 size="${1:-320x240}"
 count="${2:-100}"
+word="${3:-$(shuf /usr/share/dict/words | head -1)}"
 
 echo "$word"
 source=$(imgs-fetch-google "$word" "$count")
