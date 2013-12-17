fd889d02eceab598d175d1be527402f08cf3e548
Juan Manuel Borges Caño <juanmabcmail@gmail.com>
2013-12-17 14:39:11 +0100

    Auto Document!

--- dist/codemiscs.in.spec
+++ dist/codemiscs.in.spec
@@ -24,6 +24,7 @@ Requires: espeak
 
 %description
 Code Miscellaneous
+[@]pkgexplanation[@]
 
 %prep
 %setup -q
--- tw/mythes.in.sh
+++ tw/mythes.in.sh
@@ -207,12 +207,13 @@ function mythes_main
 cmd="mythes"
 cmd_name="mythes"
 cmd_description="mythes synonyms database tool"
+cmd_explanation="mythes is a command that shows synonyms for a given word."
 cmd_version="[@]pkgversion[@]"
 cmd_author="[@]pkgauthor[@]"
 cmd_bugreport="[@]pkgbugreport[@]"
 cmd_homepage="[@]pkghomepage[@]"
-cmd_usage="$cmd [OPTIONS] [DICTIONARY]"
-cmd_example="$cmd en hello"
+cmd_usage="$cmd [OPTIONS] [DICTIONARY] [TERM]"
+cmd_examples=("$cmd en hello")
 cmd_options=("/l/list/list available databases/mythes_option_list/")
 if (( "[@]pkghaveaspell[@]" == "0" ))
 then
