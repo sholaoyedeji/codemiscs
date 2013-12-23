3f86e4c4b4e691c1923e5889b44ce595e9240fb2
Juan Manuel Borges Caño <juanmabcmail@gmail.com>
2013-12-23 01:54:37 +0100

    Polish!

--- doc/gen.sh
+++ doc/gen.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-for command in cmd mkproject mythes tw rf imgs typewriter colorize 
+for command in cmd mkproject mythes tw rf imgs typewriter colorize show
 do
 	file="${command}.in.man"
 	echo "Generating $file"
