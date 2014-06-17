e378fb98f8e18d771b18493db53f6ae1cd1cb1df
Juan Manuel Borges Caño <juanmabcmail@gmail.com>
2014-06-17 09:40:21 +0200

    RunCMD!

--- doc/stage2/runcmd.sh
+++ doc/stage2/runcmd.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
-printf "%s" "RunCMD By Juan Manuel Borges Caño" | colorize fancy
-printf "%s" "$@" | colorize smart
+printf "%s\n" "RunCMD By Juan Manuel Borges Caño" | colorize fancy
+printf "%s\n" "$@" | colorize smart
 "$@"
 status $? --visible --audible
 printf "%s\n" 'Job Done!' | colorize fancy
